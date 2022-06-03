e49c4d28251133854c98ae67b474e3d80babe0e5
bagoferasers <ctrull60@gmail.com>
2022-06-03 16:42:14 -0500

    hurtbox & hitbox start

--- World/Grass.gd
+++ World/Grass.gd
@@ -1,12 +1,13 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("attack"):
+func create_grass_effect():
 		# GrassEffect is the scene, grassEffect is a node with an instance of the scene
 		var GrassEffect = load("res://Effects/GrassEffect.tscn")
 		var grassEffect = GrassEffect.instance()
 		var world = get_tree().current_scene
 		world.add_child(grassEffect)
 		grassEffect.global_position = global_position
-		# adds node to list to be removed from game
-		queue_free()
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
