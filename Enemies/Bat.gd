b2917a3b8c35bf637b7a30c5d5739cb271bcdb22
bagoferasers <ctrull60@gmail.com>
2022-07-11 13:48:35 -0500

    updated effects
    updated enemy death effect, grass effect, and hit effect to all use the same script

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,20 +1,21 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
+
 var knockback = Vector2.ZERO
 
 onready var stats = $Stats
-
-func _ready():
-	print(stats.max_health)
-	print(stats.health)
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
 	knockback = move_and_slide(knockback)
 
 func _on_Hurtbox_area_entered(area):
-	stats.health -= 1
-	if stats.health <= 0:
-		queue_free()
-	print(stats.health)
+	stats.health -= area.damage
 	knockback = area.knockback_vector * 100 
+
+func _on_Stats_no_health():
+	queue_free()
+	var enemyDeathEffect = EnemyDeathEffect.instance()
+	get_parent().add_child(enemyDeathEffect)
+	enemyDeathEffect.global_position = global_position
--- Player/Player.gd
+++ Player/Player.gd
@@ -13,7 +13,7 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
-var roll_vector = Vector2.LEFT
+var roll_vector = Vector2.DOWN
 
 # doesn't need ready function
 onready var animationPlayer = $AnimationPlayer
